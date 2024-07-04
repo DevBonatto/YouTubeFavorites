305ed882e1f610535e652f9c81704421e268f521
felipezws <felipezwaizdis@gmail.com>
2024-07-03 22:24:58 -0300

    working fetch

--- src/felipe.js
+++ src/felipe.js
@@ -1,3 +1,6 @@
-export function felipe() {
+export async function felipe() {
   console.log('teste Felipe')
+  let htmlText = await (await fetch("https://www.youtube.com/@gamefromscratch")).text()
+  console.log(htmlText)
+  console.log(htmlText.substring(htmlText.indexOf("subscribers") - 5, htmlText.indexOf("subscribers") - 2))
 }
